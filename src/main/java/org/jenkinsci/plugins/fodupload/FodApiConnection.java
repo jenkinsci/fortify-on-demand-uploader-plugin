72a21de19bff1e70508a5608bb023f7dbbf3c97e
Matt Gibbs <matt.gibbs@microfocus.com>
2017-11-09 11:01:59 -0500

    make FodApiConnection package-private

--- src/main/java/org/jenkinsci/plugins/fodupload/FodApiConnection.java
+++ src/main/java/org/jenkinsci/plugins/fodupload/FodApiConnection.java
@@ -35,7 +35,7 @@ public class FodApiConnection {
      * @param secret  apiConnection secret
      * @param baseUrl apiConnection url
      */
-    public FodApiConnection(final String key, final String secret, final String baseUrl, final String apiUrl) {
+    FodApiConnection(final String key, final String secret, final String baseUrl, final String apiUrl) {
         this.key = key;
         this.secret = secret;
         this.baseUrl = baseUrl;
@@ -81,7 +81,7 @@ public class FodApiConnection {
     /**
      * Retire the current token. Unclear if this actually does anything on the backend.
      */
-    public void retireToken() throws IOException {
+    void retireToken() throws IOException {
 
         Request request = new Request.Builder()
                 .url(apiUrl + "/oauth/retireToken")
