1d2bea5b21fa80297b7ff88c12785b5aecde310d
aishkotni <47675035+aishkotni@users.noreply.github.com>
2021-09-09 15:03:11 -0400

    Adding a check to warn/fail build based on print pass/fail policy (#65)
    * Adding a cehck to pass/fail build based on print pas/fil
    
    * Update StatusPollerThread.java
    
    Added back deleted status

--- src/main/java/org/jenkinsci/plugins/fodupload/polling/StatusPollerThread.java
+++ src/main/java/org/jenkinsci/plugins/fodupload/polling/StatusPollerThread.java
@@ -88,12 +88,15 @@ class StatusPollerThread extends Thread {
                 result.setPassing(true);
             }
             if (finished) {
-                result.setPassing(true);
+                if(statusString.equals(AnalysisStatusTypeEnum.Completed.name())){
+                    result.setPassing(pollingSummaryDTO.getPassFailStatus());
+                }else {
+                    result.setPassing(true);
+                }
                 result.setPollingSuccessful(true);
                 if (!statusString.equals(AnalysisStatusTypeEnum.Waiting.name())) {
                         result.setFailReason(pollingSummaryDTO.getPassFailReasonType());
                 }
-                
             }
         }
     }
